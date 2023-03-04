55da8dc53b27af7c7fa3e0a169125109156cc4d7
İrem Erim <iremerim34@gmail.com>
2023-03-04 22:35:51 +0300

    ilk commit

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link';
+import Date from '../components/date';
+import { useState } from 'react';
+
+export default function Home({ allPostsData }) {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h1>deneme</h1>
+
+
+
+        <button onClick={handleClick}>Like ({likes})</button>
+        <h1>test</h1>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
---
+++ posts/hello.md
@@ -0,0 +1,4 @@
+---
+title: 'When to Use Stdsafıjskjfsdlajkadsllfjsdkaslşfjfdklsşkvjdflsşfkjdkslaşfvkjdlsşfkjkdlsşjvkldkvflds. Server-side Rendering'
+date: '2022-01-02'
+---
